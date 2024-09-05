46200820ab567c45215b9a0a064a06ab34a85fbd
Dhruv Burada <buradaddhruv35@gmail.com>
2024-09-05 09:38:39 +0530

    fixed even odd

--- EvenOddCheck.java
+++ EvenOddCheck.java
@@ -5,7 +5,7 @@ public class EvenOddCheck {
         if (number % 2 == 0) {
             System.out.println("The number is even.");
         } else {
-            System.out.println("The number is odd.);
+            System.out.println("The number is odd.");
         }
     }
 }
